7497be234e6c678ab8eda3d852c78c8caacc3305
Scorpio962b <samaaiab962@gmail.com>
2023-05-16 16:09:29 +0200

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+let myName= "abdoel";
+let mySurname= "samaai";
+let fullName = myName + ' ' + mySurname ;
+console.log(fullName);
+
+
+let numberVar = "5"
+console.log(numberVar)
+
+const Fullname = "abdoelmaaliek"
+console.log(Fullname)
+
+Fullname ="surName"
